dec415bf8a6fd2ddfe7946be8ce360fbf27a995f
younes <younes.barrad@77diamonds.com>
2022-04-16 17:08:07 +0000

    ⚙️updated scroll delay

--- server-middleware/website-shot.js
+++ server-middleware/website-shot.js
@@ -11,7 +11,7 @@ async function autoScroll(page) {
   await page.evaluate(async () => {
     await new Promise((resolve, reject) => {
       var totalHeight = 0;
-      var distance = 100;
+      var distance = 200;
       var timer = setInterval(() => {
         var scrollHeight = document.body.scrollHeight;
         window.scrollBy(0, distance);
@@ -21,7 +21,7 @@ async function autoScroll(page) {
           clearInterval(timer);
           resolve();
         }
-      }, 100);
+      }, 300);
     });
   });
 }
